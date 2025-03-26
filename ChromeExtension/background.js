443b9f59db0eef2b921c244bd38327ee99349eb8
luanakwon <junnyboy99@gmail.com>
2025-03-26 12:05:00 -0700

    moved alert/confirm from popup.js to chrome.scripting

--- ChromeExtension/background.js
+++ ChromeExtension/background.js
@@ -34,11 +34,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                         if (chrome.runtime.lastError.message.startsWith("Could not establish connection")){
                             console.log("no connection");
                             currentState = 0;
-                            chrome.runtime.sendMessage({
-                                action: "popup_alert",
-                                message: "Please navigate to \"Purchase Order Worksheet\" to start the process.",
-                                state: currentState
-                            });
+                            requestUIPopUP('alert',
+                                "Please navigate to \"Purchase Order Worksheet\" to start the process."
+                            );
+                            setStartButtonActive(true);
                         }
                     }
                 });
@@ -46,11 +45,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                 if (name === 'TypeError') {
                     console.log("no accessible tab");
                     currentState = 0;
-                    chrome.runtime.sendMessage({
-                        action: "popup_alert",
-                        message: "Please navigate to \"Purchase Order Worksheet\" to start the process.",
-                        state: currentState
-                    });
+                    requestUIPopUP('alert',
+                        "Please navigate to \"Purchase Order Worksheet\" to start the process."
+                    );
+                    setStartButtonActive(true);
                 }
             }
         });
@@ -71,11 +69,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             console.log(message);
             // start condition faild. alert.
             currentState = 0;
-            chrome.runtime.sendMessage({
-                action: "popup_alert",
-                message: "Please navigate to \"Purchase Order Worksheet\" to start the process.",
-                state: currentState
-            });
+            requestUIPopUP('alert',
+                "Please navigate to \"Purchase Order Worksheet\" to start the process."
+            );
+            setStartButtonActive(true);
         }
     }
 
@@ -89,58 +86,48 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         if (n_selection > 0){
             currentState = 3; // confirmSelection
             // confirm selection
-            chrome.runtime.sendMessage(
-                {
-                    action: "popup_confirm",
-                    message: `${n_selection} invoices selected. Would you like to proceed?`,
-                    state: currentState
-                }, (response) => {
-                    if (response.confirmResult){
-                        // save list
-                        console.log("saving selectedInvoiceList");
-                        console.log(selectedInvoiceList);
-                        console.log()
-                        chrome.storage.session.set(
-                            {selectedInvoices:JSON.stringify(selectedInvoiceList)}
-                        ).then(()=>{
-                            // save each item
-                            console.log("saving each item from selectedInvoiceMap");
-                            return Promise.all([
-                                selectedInvoiceList.map(name=>
-                                    chrome.storage.session.set(
-                                        Object.fromEntries(
-                                            [[name,JSON.stringify(selectedInvoiceMap.get(name))]]
-                                        )
+            requestUIPopUP('confirm',
+                `${n_selection} invoices selected. Would you like to proceed?`
+            ).then((confirmResult)=>{
+                if (confirmResult) {
+                    // save list
+                    console.log("saving selectedInvoiceList");
+                    console.log(selectedInvoiceList);
+                    console.log()
+                    chrome.storage.session.set(
+                        {selectedInvoices:JSON.stringify(selectedInvoiceList)}
+                    ).then(()=>{
+                        // save each item
+                        console.log("saving each item from selectedInvoiceMap");
+                        return Promise.all([
+                            selectedInvoiceList.map(name=>
+                                chrome.storage.session.set(
+                                    Object.fromEntries(
+                                        [[name,JSON.stringify(selectedInvoiceMap.get(name))]]
                                     )
                                 )
-                            ]);
-                        }).then(()=>{
-                            currentState = 4; // export
-                            // start sequential export
-                            // expected answer: exportSequenceResult   
-                            Exporter.startExportSequence();
-                        });
-                    } else {
-                        // selection denied
-                        // to state idle w/o additional message
-                        currentState = 0;
-                        chrome.runtime.sendMessage({
-                            action: "popup_nomsg",
-                            state: currentState
-                        });
-                    }
-                });
+                            )
+                        ]);
+                    }).then(()=>{
+                        currentState = 4; // export
+                        // start sequential export
+                        // expected answer: exportSequenceResult   
+                        Exporter.startExportSequence();
+                    });
+                } else {
+                    currentState = 0;
+                    setStartButtonActive(true);
+                }
+            });
         } else {
             // zero invoices selected 
             currentState = 0;
-            chrome.runtime.sendMessage({
-                action: "popup_alert",
-                message: "Please select at least 1 invoice to continue",
-                state: currentState
-            });
+            requestUIPopUP('alert',
+                "Please select at least 1 invoice to continue"
+            );
+            setStartButtonActive(true);
         }
     }
-    
     // unhandled message
     else {
         console.log(`Unexpected Message(background.js/runtime/${currentState}): ${message.action}`);
@@ -193,51 +180,17 @@ async function loadAndOrganizeExportedResult(){
             Object.fromEntries([[name, JSON.stringify(invoice)]]));
     }
     // next state = confirm import
-    console.log(`
-        TODO here. whenever something is clicked (chrome message, webpage, etc) popup.html would disappear, resulting lost connection. 
-        I'm not sure whether I should move such user confirmation to content.js,
-        or just use chrome.executeScript.`);
-
-    chrome.runtime.sendMessage(
-        {
-            action: "popup_confirm",
-            message: 'Would you also like to update your system?',
-            state: currentState
-        }, (response) => {
-            if (
-                !response ||
-                (chrome.runtime.lastError && chrome.runtime.lastError.message.startsWith(
-                    "Could not establish connection"))
-            ){
-                console.log("no connection");
-                chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                    chrome.scripting.executeScript({
-                        target: { tabId: tabs[0].id },
-                        func: () => confirm("Would you also like to update your system?")
-                    }).then(results => {
-                        const userConfirmed = results[0].result; // Fetch the returned value
-                        if (userConfirmed) {
-                            currentState = 6; // Set state to import
-                            Importer.startImportSequence();
-                        } else {
-                            openResultsAndFinishProcess();
-                        }
-                    });
-                });
-            }
-            else if (response.confirmResult){
-                // current state = 6: import
-                currentState = 6;
-                // start sequantial import                    
-                // expected answer: importSequenceResult  
-                Importer.startImportSequence(); 
-            } else {
-                // import dismissed
-                openResultsAndFinishProcess();
-            }
-        }
-    );
     
+    requestUIPopUP('confirm',
+        'Would you also like to update your system?'
+    ).then((confirmResult)=>{
+        if (confirmResult) {
+            currentState = 6; // Set state to import
+            Importer.startImportSequence();
+        } else {
+            openResultsAndFinishProcess();
+        }
+    });    
 }
 
 function openResultsAndFinishProcess(){
@@ -248,8 +201,43 @@ function openResultsAndFinishProcess(){
     
     // back to idle state
     currentState = 0;
-    chrome.runtime.sendMessage({
-        action: "popup_nomsg",
-        state: currentState
+    setStartButtonActive(true);
+}
+
+function requestUIPopUP(type, message){
+    return new Promise((resolve, reject) => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                func: (type, message) => {
+                    if (type === 'alert'){
+                        alert(message);
+                    } 
+                    else if (type === 'confirm') {
+                        return confirm(message);
+                    } else {
+                        console.log(`No such PopUp type (type=${type}).`);
+                    }
+                    return;
+                },
+                args: [type, message]
+            }).then(results => {
+                if (type === 'confirm') {
+                    resolve(results[0].result);
+                }
+                else {
+                    resolve();
+                }
+            }).catch((err) => reject(err));
+        });
     });
+}
+function setStartButtonActive(flag){
+    // send message to popup.js to set the start button
+    // when popup.html is inactive, the message will be lost
+    // in that case, popup.html will update itself when activated
+    chrome.runtime.sendMessage({
+        action: 'popup_setButton',
+        setActive: flag
+    })
 }
--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -24,21 +24,11 @@ startButton.addEventListener("click", ()=>{
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if  (message.action === 'popup_alert') {
-        alert(message.message);
-        if (message.state === 0) {startButton.disabled = false;}
-    }
-    else if (message.action === 'popup_confirm') {
-        sendResponse({confirmResult: confirm(message.message)});
-    }
-    else if (message.action === 'popup_nomsg') {
-        if (message.state === 0) {startButton.disabled = false;}
+    if  (message.action === 'popup_setButton'){
+        startButton.disabled = !message.setActive;
     }
     // unhandled message
     else {
         console.log(`Unexpected Message(popup.js/runtime/-): ${message.action}`);
     }
 });
-
-
-
